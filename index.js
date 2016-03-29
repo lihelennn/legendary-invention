a6a75e61d1c67b6fa120646553d967b1baadeb94
Liam Daly <liamgdalynyc@gmail.com>
2016-03-29 09:41:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var dem_data = [ {state: "Iowa", respondents: 1660, men: "43%", women: "57%", me
 	 {state: "South Carolina", respondents: 1261, men: "39%", women: "61%", menC: 68, womenC: 79, menS: 32, womenS: 21, under: "61%", over: "39%", underC: 76, overC: 68, underS: 23, overS: 32, winner: "Clinton" },
 	 {state: "Alabama", respondents: 865, men: "40%", women: "60%", menC: 73, womenC: 80, menS: 24, womenS: 17, under: "55%", over: "45%", underC: 80, overC: 76, underS: 17, overS: 23, winner: "Clinton" },
 	 {state: "Arkansas", respondents: 944, men: "43%", women: "57%", menC: 60, womenC: 76, menS: 37, womenS: 23, under: "51%", over: "49%", underC: 69, overC: 69, underS: 29, overS: 29, winner: "Clinton" },
-	 {state: "Georgia", respondents: 1491, men: "38%", women: "62%", menC: 66, womenC: 576, menS: 33, womenS: 23, under: "43%", over: "57%", underC: 70, overC: 72, underS: 29, overS: 28, winner: "Clinton" },
+	 {state: "Georgia", respondents: 1491, men: "38%", women: "62%", menC: 66, womenC: 76, menS: 33, womenS: 23, under: "43%", over: "57%", underC: 70, overC: 72, underS: 29, overS: 28, winner: "Clinton" },
 	 {state: "Massachusetts", respondents: 1406, men: "42%", women: "58%", menC: 41, womenC: 57, menS: 58, womenS: 42, under: "31%", over: "69%", underC: 43, overC: 53, underS: 56, overS: 46, winner: "Clinton" },
 	 {state: "Oklahoma", respondents: 821, men: "46%", women: "54%", menC: 33, womenC: 48, menS: 60, womenS: 46, under: "44%", over: "56%", underC: 41, overC: 51, underS: 54, overS: 51, winner: "Sanders" }, ];
 
