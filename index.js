6c23d37cc476473f5f19c0ac5af0baeabf5ebba4
Felipe Mansilla <pipe.ny@gmail.com>
2016-03-30 00:08:32 -0400

    nothings working

--- index.js
+++ index.js
@@ -69,14 +69,123 @@ var dem_data = [ {state: "Iowa", respondents: 1660,
 		  grad: "49%", nograd: "51%", 
 		  gradC: 45, nogradC: 36, 
 		  gradS: 50, nogradS: 55, 
-		  winner: "Sanders" }, ];
+		  winner: "Sanders" } ];
+
+var rep_data = [ {state: "Iowa", respondents: 1794, 
+		  men: "52%", women: "48%", 
+		  menC: 29, womenC: 27, 
+		  menK: 2, womenK: 2, 
+		  menR: 25, womenR: 21, 
+		  menT: 25, womenT: 24, 
+		  grad: "51%", nograd: "49%", 
+		  gradC: 25, nogradC: 31, 
+		  gradK: 3, nogradK: 1, 
+		  gradR: 28, nogradR: 17, 
+		  gradT: 21, nogradT: 28, 
+		  winner: "Cruz" },
+		 {state: "New Hampshire", respondents: 2036, 
+		  men: "52%", women: "48%", 
+		  menC: 12, womenC: 11, 
+		  menK: 16, womenK: 16, 
+		  menR: 9, womenR: 12, 
+		  menT: 38, womenT: 33, 
+		  grad: "53%", nograd: "47%", 
+		  gradC: 11, nogradC: 13, 
+		  gradK: 19, nogradK: 12, 
+		  gradR: 12, nogradR: 10, 
+		  gradT: 30, nogradT: 42, 
+		  winner: "Trump" },
+		 {state: "Nevada", respondents: 1573, 
+		  men: "52%", women: "48%", 
+		  menC: 23, womenC: 20, 
+		  menK: 3, womenK: 4, 
+		  menR: 22, womenR: 25, 
+		  menT: 47, womenT: 45, 
+		  grad: "49%", nograd: "51%", 
+		  gradC: 21, nogradC: 22, 
+		  gradK: 5, nogradK: 3, 
+		  gradR: 28, nogradR: 20, 
+		  gradT: 40, nogradT: 51, 
+		  winner: "Trump" },
+		 {state: "South Carolina", respondents: 1800, 
+		  men: "50%", women: "50%", 
+		  menC: 37, womenC: 39, 
+		  menK: 12, womenK: 14, 
+		  menR: 5, womenR: 9, 
+		  menT: 43, womenT: 36, 
+		  grad: "51%", nograd: "49%", 
+		  gradC: 38, nogradC: 38, 
+		  gradK: 18, nogradK: 8, 
+		  gradR: 8, nogradR: 6, 
+		  gradT: 33, nogradT: 46, 
+		  winner: "Trump" },
+		 {state: "Alabama", respondents: 1237, 
+		  men: "49%", women: "51%", 
+		  menC: 22, womenC: 21, 
+		  menK: 4, womenK: 5, 
+		  menR: 14, womenR: 23, 
+		  menT: 52, womenT: 36, 
+		  grad: "44%", nograd: "56%", 
+		  gradC: 22, nogradC: 21, 
+		  gradK: 7, nogradK: 2, 
+		  gradR: 23, nogradR: 15, 
+		  gradT: 35, nogradT: 51, 
+		  winner: "Trump" },
+		 {state: "Arkansas", respondents: 1382, 
+		  men: "52%", women: "48%", 
+		  menC: 29, womenC: 30, 
+		  menK: 4, womenK: 4, 
+		  menR: 23, womenR: 28, 
+		  menT: 37, womenT: 31, 
+		  grad: "45%", nograd: "55%", 
+		  gradC: 28, nogradC: 31, 
+		  gradK: 6, nogradK: 2,
+		  gradR: 31, nogradR: 21, 
+		  gradT: 28, nogradT: 39,
+		  winner: "Trump" },
+		 {state: "Georgia", respondents: 1704, 
+		  men: "49%", women: "51%", 
+		  menC: 22, womenC: 24, 
+		  menK: 6, womenK: 6, 
+		  menR: 21, womenR: 28, 
+		  menT: 45, womenT: 34, 
+		  grad: "53%", nograd: "47%", 
+		  gradC: 24, nogradC: 22, 
+		  gradK: 6, nogradK: 5, 
+		  gradR: 29, nogradR: 20, 
+		  gradT: 34, nogradT: 46, 
+		  winner: "Trump" },
+		 {state: "Massachusetts", respondents: 801, 
+		  men: "52%", women: "48%", 
+		  menC: 10, womenC: 9, 
+		  menK: 16, womenK: 19, 
+		  menR: 16, womenR: 20, 
+		  menT: 52, womenT: 46, 
+		  grad: "60%", nograd: "40%", 
+		  gradC: 10, nogradC: 8, 
+		  gradK: 24, nogradK: 10, 
+		  gradR: 20, nogradR: 14, 
+		  gradT: 40, nogradT: 62, 
+		  winner: "Trump" },
+		 {state: "Oklahoma", respondents: 938, 
+		  men: "50%", women: "50%", 
+		  menC: 34, womenC: 35, 
+		  menK: 4, womenK: 4, 
+		  menR: 24, womenR: 28, 
+		  menT: 33, womenT: 24, 
+		  grad: "49%", nograd: "51%", 
+		  gradC: 31, nogradC: 36, 
+		  gradK: 4, nogradK: 3, 
+		  gradR: 33, nogradR: 20, 
+		  gradT: 24, nogradT: 33, 
+		  winner: "Cruz" } ];
 
 var Hillary = ["Iowa", "Nevada", "South Carolina", "Alabama", "Arkansas", "Georgia", "Massachusetts", "Tennessee", "Texas", "Virginia", "Louisiana", "Mississippi", "Florida", "Illinois", "Missouri", "North Carolina", "Ohio", "Arizona"];
 
 var Sanders = ["New Hampshire", "Colorado", "Minnesota", "Oklahoma", "Vermont", "Kansas", "Nebraska", "Maine", "Michigan", "Idaho", "Utah", "Alaska", "Hawaii", "Washington"];
 
-var state = "Iowa"
-var category = "Gender";
+//var state = "Iowa"
+//var category = "Gender";
 var data;
 var dataCategory = [];
 var dataCategory2 = [];
@@ -113,6 +222,8 @@ function getData(){
 
     if(category == "Gender"){
 	dataCategory.push(data["men"]);
+	infoArray[3] = "Men: " + data["men"];
+	infoArray[4] = "Women: " + data["women"];
 	dataCategory.push(data["menC"]);
 	dataCategory.push(data["menS"]);
 	dataCategory.push(data["women"]);
@@ -125,6 +236,8 @@ function getData(){
 	dataCategory2.push("womenC: " + dataCategory[4]);
 	dataCategory2.push("womenS: " + dataCategory[5]);
     }else{
+	infoArray[3] = "College Grads: " + data["grad"];
+	infoArray[4] = "Non-College Grads: " + data["nograd"];
 	dataCategory.push(data["grad"]);
 	dataCategory.push(data["gradC"]);
 	dataCategory.push(data["gradS"]);
@@ -159,6 +272,7 @@ function go(){
 */
 
     
+    
     getData();
     var infoDem = d3.select(".infoDem");
     var info = infoDem.selectAll("h3");
@@ -179,6 +293,7 @@ function go(){
     barEnter.text(function(d){
 	return d;
     });
+
 }
 
 
